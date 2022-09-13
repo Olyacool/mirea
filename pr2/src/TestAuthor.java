81fea4e897720a8061a2b4112eec529ae58ccae7
Nazim <105489079+Olyacool@users.noreply.github.com>
2022-09-13 19:22:06 +0300

    Add files via upload

---
+++ pr2/src/TestAuthor.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+public class TestAuthor {
+    public static void main(String[] args) {
+      Author c1 = new Author("Shespear", "Sheaspear121@gmail.com", 'M');
+      Author c2 = new Author("Julien", "Julien4456@yandex.ru", 'V');
+        System.out.println(c1);
+        System.out.println(c2);
+        c2.setEmail(" Jusien2839@mail.com");
+
+    }
+}
