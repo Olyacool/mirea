7bd0b649ed59fee83a6306de98f1deda84537ec6
Nazim <105489079+Olyacool@users.noreply.github.com>
2022-11-26 15:52:47 +0300

    Add files via upload

---
+++ Naz/src/Der.java
@@ -0,0 +1,13 @@
+public class Der {
+    public static String recursion(int n) {
+
+        if (n < 10) {
+            return Integer.toString(n);}
+        else{
+            return recursion(n / 10) + " " + n % 10;}
+    }
+
+    public static void main(String[] args) {
+        System.out.println(recursion(123));
+    }
+}
